a47b0b2816106aa3062421abf89be31fe395d528
thisisrow <prathameshmisra2020@gmail.com>
2025-08-23 23:25:13 +0530

    add cors

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const morgan = require("morgan");
+const cors = require('cors'); 
 const userRoutes = require('./routes/userroutes');
 const connectDB = require('./connection/db');
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors()); 
 app.use(express.json());
 
 // Connect to Database
 connectDB();
 app.use(morgan("dev"));
+
 // Routes
 app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
     res.send("hello world");
 });
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
